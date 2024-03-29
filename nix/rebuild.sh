d9c7f320c740393e2403eb3ee94b63bd1cef7565
Cristian Oliveira <contato@cristianoliveira.com.br>
2024-03-29 09:54:36 +0100

    chore(nix): adds context to rebuild script

--- nix/rebuild.sh
+++ nix/rebuild.sh
@@ -17,13 +17,13 @@ if [ -z "$SKIP_COMMIT" ]; then
 fi
 
 if [ "$(uname)" = "Darwin" ]; then
-  # OSX
+  # OSX (darwin)
   echo "Rebuilding Darwin configuration..."
-  NIXOS_CONFIG=$HOME/.dotfiles/nix#darwin
+  NIXOS_CONFIG=$HOME/.dotfiles/nix
   darwin-rebuild switch --flake $HOME/.dotfiles/nix#darwin
 else
-  # Linux
-  NIXOS_CONFIG=$HOME/.dotfiles/nix#nixos
+  # NixOS (linux)
+  NIXOS_CONFIG=$HOME/.dotfiles/nix
   echo "Rebuilding NixOS configuration..."
   sudo nixos-rebuild switch --flake $HOME/.dotfiles/nix#nixos
 fi
